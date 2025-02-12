0eb2952b0c3c14e470e3ae13f4805283afc3963c
cyrine_sa <cyrineSA@hotmail.com>
2025-02-12 13:03:51 +0100

    Commit test

--- arch/arch.js
+++ arch/arch.js
@@ -15,11 +15,7 @@ fetch(navigationHtml)
     .then(response => response.text())
     .then(html => document.getElementById("navigation").innerHTML = html)
 
-
-<<<<<<< HEAD
-=======
     
->>>>>>> b94ac1129a517b4210094246b07fdd6db83183e9
 fetch("../arch/footer.html")
     .then(response => response.text())
     .then(html => document.getElementById("footer").innerHTML = html)
